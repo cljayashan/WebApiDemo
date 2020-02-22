a6a49851e5b60e054a3edede24d854a9830ffa1d
Jayashan Liyanage <laksrijayashan@gmail.com>
2020-02-23 00:15:57 +0530

    Created GetAll method for student controller

---
+++ WebApi2/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi2.ViewModels;
+
+namespace WebApi2.Controllers
+{
+    public class StudentController : ApiController
+    {
+        WebApi2.Models.WebApiDemoEntities db = new Models.WebApiDemoEntities();
+        
+        [HttpPost]
+        [ActionName ("GetAll")]
+        public HttpResponseMessage  getAllStudents()
+        {
+            List<Student> data = db.TBL_STUDENT.Select(y => new Student
+            {
+                Id = y.Id,
+                FirstName = y.FirstName,
+                LastName = y.LastName,
+                Gender = y.Gender ?? 0,
+                DOB = y.DOB ?? DateTime.MinValue
+            }).ToList();
+
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+    }
+}
---
+++ WebApi2/ViewModels/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi2.ViewModels
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Gender { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
